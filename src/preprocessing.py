8c96c84e6990d82928905be6bf47a8da37699217
Adrian Kowal <adrian2002kowal@gmail.com>
2025-04-21 12:54:18 +0200

    ADD preprocessing pipeline: encoding and scaling

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 
 def convert_categorical_dtype(df: pd.DataFrame) -> pd.DataFrame:
 
@@ -15,9 +17,44 @@ def convert_categorical_dtype(df: pd.DataFrame) -> pd.DataFrame:
             df[col] = df[col].astype('category')
     return df
 
+def build_preprocessing_pipeline() -> ColumnTransformer:
+    ordinal_cols = ['EducationLevel']
+    nominal_cols = [
+        'Gender', 'Ethnicity', 'Smoking', 'FamilyHistoryAlzheimers',
+        'CardiovascularDisease', 'Diabetes', 'Depression', 'HeadInjury',
+        'Hypertension', 'MemoryComplaints', 'BehavioralProblems',
+        'Confusion', 'Disorientation', 'PersonalityChanges',
+        'DifficultyCompletingTasks', 'Forgetfulness'
+    ]
+    numeric_cols = [
+        'Age', 'BMI', 'AlcoholConsumption', 'PhysicalActivity',
+        'DietQuality', 'SleepQuality', 'SystolicBP', 'DiastolicBP',
+        'CholesterolTotal', 'CholesterolLDL', 'CholesterolHDL',
+        'CholesterolTriglycerides', 'MMSE', 'FunctionalAssessment', 'ADL'
+    ]
+
+
+    ordinal_transformer = OrdinalEncoder()
+    nominal_transformer = OneHotEncoder(drop='first', sparse_output=False)
+    scaler = StandardScaler()
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('old', ordinal_transformer, ordinal_cols),
+            ('nom', nominal_transformer, nominal_cols),
+            ('num', scaler, numeric_cols)
+        ],
+        remainder='drop'
+    )
+    return preprocessor
+
 
 if __name__ == "__main__":
     from src.data_import import load_data
     df = load_data("../data/alzheimers_disease_data.csv")
     df = convert_categorical_dtype(df)
-    print(df.dtypes[df.dtypes == 'category'])
+    preproc = build_preprocessing_pipeline()
+    X = df.drop(columns=['PatientID', 'Diagnosis', 'DoctorInCharge'])
+    X_trans = preproc.fit_transform(X)
+    print("Oryginal shape:", X.shape)
+    print("Transformed shape:", X_trans.shape)
